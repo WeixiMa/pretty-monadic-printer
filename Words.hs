da092df37531d43bf58cf48939c9987e834e7b35
mvc <mvcccccc@gmail.com>
2017-04-08 16:51:05 -0400

    aahh

---
+++ Words.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Words where
+
+import Control.Monad
+import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
+import Control.Monad.RWS
+import Data.List
+import Data.Text (Text)
+import qualified Data.Text as T
+
+import Pretty
+
+equals :: (MonadPretty w ann fmt m) => m ()
+equals = char '='
+
+colon :: (MonadPretty w ann fmt m) => m ()
+equals = char ':'
